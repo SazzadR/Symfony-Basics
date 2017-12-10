0d319a0defa492aa6b92c64127aa2ca5a406a173
Sazzadur Rahman <sazzad.rahman.bd@gmail.com>
2017-12-10 22:03:58 +0600

    Custom query

--- src/AppBundle/Entity/Genus.php
+++ src/AppBundle/Entity/Genus.php
@@ -5,7 +5,7 @@ namespace AppBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\GenusRepository")
  * @ORM\Table(name="genus")
  */
 class Genus
@@ -36,6 +36,11 @@ class Genus
      * @ORM\Column(type="string", nullable=true)
      */
     private $funFact;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isPublished;
 
     public function getId()
     {
@@ -80,6 +85,14 @@ class Genus
     public function setFunFact($funFact)
     {
         $this->funFact = $funFact;
+    }
+
+    /**
+     * @param mixed $isPublished
+     */
+    public function setIsPublished($isPublished): void
+    {
+        $this->isPublished = $isPublished;
     }
 
     public function getLastUpdatedAt()
